7a148598f30aa4952e3602aca2ddb00a89e17324
Jerry <midsmr@qq.com>
2022-08-30 06:35:07 +0800

    assets init

---
+++ resources/dist/js/ui-jp.config.js
@@ -0,0 +1,84 @@
+// lazyload config
+
+var jp_config = {
+    easyPieChart: [
+        '../lib/jquery.easy-pie-chart/dist/jquery.easypiechart.fill.js',
+    ],
+    sparkline: [
+        '../lib/jquery.sparkline/dist/jquery.sparkline.retina.js'
+    ],
+    plot: [
+        '../lib/flot/jquery.flot.js',
+        '../lib/flot/jquery.flot.pie.js',
+        '../lib/flot/jquery.flot.resize.js',
+        '../lib/flot.tooltip/js/jquery.flot.tooltip.min.js',
+        '../lib/flot.orderbars/js/jquery.flot.orderBars.js',
+        '../lib/flot-spline/js/jquery.flot.spline.min.js'
+    ],
+    moment: [
+        '../lib/moment/moment.js'
+    ],
+    screenfull: [
+        '../lib/screenfull/dist/screenfull.min.js'
+    ],
+    slimScroll: [
+        '../lib/slimscroll/jquery.slimscroll.min.js'
+    ],
+    sortable: [
+        '../lib/html5sortable/jquery.sortable.js'
+    ],
+    nestable: [
+        '../lib/nestable/jquery.nestable.js',
+        '../lib/nestable/jquery.nestable.css'
+    ],
+    filestyle: [
+        '../lib/bootstrap-filestyle/src/bootstrap-filestyle.js'
+    ],
+    slider: [
+        '../lib/bootstrap-slider/bootstrap-slider.js',
+        '../lib/bootstrap-slider/bootstrap-slider.css'
+    ],
+    chosen: [
+        '../lib/chosen/chosen.jquery.min.js',
+        '../lib/chosen/bootstrap-chosen.css'
+    ],
+    TouchSpin: [
+        '../lib/bootstrap-touchspin/dist/jquery.bootstrap-touchspin.min.js',
+        '../lib/bootstrap-touchspin/dist/jquery.bootstrap-touchspin.min.css'
+    ],
+    wysiwyg: [
+        '../lib/bootstrap-wysiwyg/bootstrap-wysiwyg.js',
+        '../lib/bootstrap-wysiwyg/external/jquery.hotkeys.js'
+    ],
+    dataTable: [
+        '../lib/datatables/media/js/jquery.dataTables.min.js',
+        '../lib/plugins/integration/bootstrap/3/dataTables.bootstrap.js',
+        '../lib/plugins/integration/bootstrap/3/dataTables.bootstrap.css'
+    ],
+    vectorMap: [
+        '../lib/bower-jvectormap/jquery-jvectormap-1.2.2.min.js',
+        '../lib/bower-jvectormap/jquery-jvectormap-world-mill-en.js',
+        '../lib/bower-jvectormap/jquery-jvectormap-us-aea-en.js',
+        '../lib/bower-jvectormap/jquery-jvectormap.css'
+    ],
+    footable: [
+        '../lib/footable/v3/js/footable.min.js',
+        '../lib/footable/v3/css/footable.bootstrap.min.css'
+    ],
+    fullcalendar: [
+        '../lib/moment/moment.js',
+        '../lib/fullcalendar/dist/fullcalendar.min.js',
+        '../lib/fullcalendar/dist/fullcalendar.css',
+        '../lib/fullcalendar/dist/fullcalendar.theme.css'
+    ],
+    daterangepicker: [
+        '../lib/moment/moment.js',
+        '../lib/bootstrap-daterangepicker/daterangepicker.js',
+        '../lib/bootstrap-daterangepicker/daterangepicker-bs3.css'
+    ],
+    tagsinput: [
+        '../lib/bootstrap-tagsinput/dist/bootstrap-tagsinput.js',
+        '../lib/bootstrap-tagsinput/dist/bootstrap-tagsinput.css'
+    ]
+
+};
